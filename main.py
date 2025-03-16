8921627876ee6125dafa036753286b9715cd4632
cuom0_ <cuomo.s.dev@gmail.com>
2025-03-16 21:44:52 +0100

    Added Mode selector + FIX
    - Added Mode Selector
    - Removed game.py, the game won't use multiple windows.
    - Fixed a lot of issues.

--- main.py
+++ main.py
@@ -1,42 +1,39 @@
 import customtkinter as ctk
 from PIL import Image
 import webbrowser  
-import game
-
 
 homescreen = ctk.CTk()
 homescreen.geometry("1000x600")
 ctk.set_default_color_theme("dark-blue")
-
+homescreen.resizable(False, False)
 homescreen.configure(fg_color="#094c7d")
 homescreen.title("pyUNO")
+
 def show_frame(frame): # A function to show a frame and hide the others
     frame.tkraise()
 
-for i in range(4): # Configuring the grid layout
+for i in range(4): # Configuring the grid layout, weight is set to 1 so the widgets will resize with the window
     homescreen.grid_columnconfigure(i, weight=1)
 
-def show_frame(frame): # A function to show a frame and hide the others
-    frame.tkraise()  
-    
 def close_program():
     homescreen.destroy()
     
 def start_game():
-    homescreen.destroy()
-    
+    game_frame = ctk.CTkFrame(homescreen, fg_color="transparent", width=1000, height=600)
+    game_frame.grid(row=0, column=0, sticky="nsew")
+    show_frame(game_frame)
 
 def open_github():
     webbrowser.open("https://github.com/cuom0")
-# Home Frame
 
+# Home Frame
 home_frame = ctk.CTkFrame(homescreen, fg_color="transparent", width=1000, height=600)
-home_frame.grid(row=0, column=0, sticky="nsew")
+home_frame.grid(row=0, column=0, sticky="nsew") # The frame is placed in the grid layout and set to fill the window
 
 title = ctk.CTkLabel(home_frame, text="pyUNO", fg_color="transparent", font=("Impact", 40), text_color="white")
 title.grid(row=0, column=0, columnspan=3, padx=30, pady=15, sticky="n")
 
-play = ctk.CTkButton(home_frame, text="Play!", font=("Arial", 45), width=400, height=90, corner_radius=20, command=start_game)
+play = ctk.CTkButton(home_frame, text="Play!", font=("Arial", 45), width=400, height=90, corner_radius=20, command=lambda: show_frame(mode_selector))
 play.grid(row=1, column=0, columnspan=3, padx=30, pady=5, sticky="n")
 
 credits = ctk.CTkButton(home_frame, text="Credits", font=("Arial", 25), width=300, height=70, corner_radius=15,
@@ -54,12 +51,10 @@ bgcardslabel = ctk.CTkLabel(home_frame, image=bgcards, text="")
 bgcardslabel.grid(row=0, column=3, rowspan=4, padx=(20, 50), pady=50, sticky="e")
 
 # Frame Credits
-
 credit_frame = ctk.CTkFrame(homescreen, fg_color="transparent", width=1000, height=600)
 credit_frame.grid(row=0, column=0, sticky="nsew")
 
 ctk.CTkLabel(credit_frame, text="Credits", font=("Impact", 40), text_color="white").pack(pady=20)
-
 
 ctk.CTkLabel(credit_frame, text="Salvatore Cuomo\n(Lead Developer, Game Designer)", font=("Arial", 30), text_color="white").pack(pady=5)
 
@@ -72,11 +67,43 @@ github_button.pack(pady=5)
 ctk.CTkLabel(credit_frame, text="Francesco Ciampa\n(Card Design, PowerPoint)", font=("Arial", 20), text_color="white").pack(pady=10)
 ctk.CTkLabel(credit_frame, text="Antonio De Cicco\n(Ideas, PowerPoint)", font=("Arial", 20), text_color="white").pack(pady=10)
 
-
 back_button = ctk.CTkButton(credit_frame, text="Back", font=("Arial", 25), width=200, height=50, corner_radius=15,
                             command=lambda: show_frame(home_frame))
 back_button.pack(pady=20)
 
+# Mode Selector Frame
+mode_selector = ctk.CTkFrame(homescreen, fg_color="transparent", width=1000, height=800)
+mode_selector.grid(row=0, column=0, sticky="nsew")
+
+classic_image = ctk.CTkImage(light_image=Image.open("./media/home/classic.png"), 
+                             dark_image=Image.open("./media/home/classic.png"),
+                             size=(100, 100))
+hard_image = ctk.CTkImage(light_image=Image.open("./media/home/hardmode.png"), 
+                          dark_image=Image.open("./media/home/hardmode.png"),
+                          size=(100, 100))
+
+mode_selector.grid_rowconfigure(0, weight=1)
+mode_selector.grid_rowconfigure(1, weight=1)
+mode_selector.grid_columnconfigure(0, weight=1)
+mode_selector.grid_columnconfigure(1, weight=1)
+mode_selector.grid_columnconfigure(2, weight=1)
+        
+gamemode_label = ctk.CTkLabel(mode_selector, text="Game Mode", font=("Impact", 40), text_color="white")
+gamemode_label.grid(row=0, column=1, pady=10, sticky="n")
+goback = ctk.CTkButton(mode_selector, text="Back", font=("Arial", 25), width=200, height=50, corner_radius=15,
+                       command=lambda: show_frame(home_frame))
+goback.grid(row=0, column=2, padx=2.5, pady=10, sticky="en")
+
+
+classic_button = ctk.CTkButton(mode_selector, text="Classic Mode", font=("Arial", 25), image=classic_image, compound="top", width=400, height=300, corner_radius=20, command=start_game)
+classic_button.grid(row=1, column=0, padx=2.5, pady=10, sticky="e")
+
+def show_coming_soon():
+    coming_soon_label = ctk.CTkLabel(mode_selector, text="Coming soon!", font=("Arial", 20), text_color="white")
+    coming_soon_label.grid(row=2, column=2, pady=10, sticky="n")
+
+hard_button = ctk.CTkButton(mode_selector, text="HARD MODE", font=("Arial", 25), text_color="red", image=hard_image, compound="top", width=400, height=300, corner_radius=20, command=show_coming_soon)
+hard_button.grid(row=1, column=2, padx=2.5, pady=0, sticky="w")
 
 home_frame.tkraise() # Makes the home frame visible by default
 
